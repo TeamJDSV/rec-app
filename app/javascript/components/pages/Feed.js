b9d229a43a021c720418239c89707dcf8837ce35
aVictorWXYZ <victorargote@gmail.com>
2022-05-26 16:05:08 -0700

    11/12 tasks completed of trello

---
+++ app/javascript/components/pages/Feed.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Feed extends Component {
+  render() {
+    return(
+      <h3>This is the Feed/Index Page</h3>
+    )
+  }
+}
+
+export default Home
