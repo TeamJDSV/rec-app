fa8070493d8b3cfea9e59d046f4e6f91af62a940
aVictorWXYZ <victorargote@gmail.com>
2022-05-26 10:30:38 -0700

    initial commit

---
+++ app/javascript/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Header from './components/Header'
+
+class App extends Component {
+  render() {
+    return(
+      <>
+        <Header {...this.props} />
+      </>
+    )
+  }
+}
+
+export default App
