fdc252a3d98abbd671ad6a1142fcf3dd4c226b96
aVictorWXYZ <victorargote@gmail.com>
2022-06-01 16:03:01 -0700

    front end edit page

---
+++ app/javascript/components/pages/EditPost.test.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import EditPost from './EditPost'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe("When EditPost renders", () => {
+    let render;
+    beforeEach(() => {
+      render = shallow(<EditPost />);
+    });
+    it("displays all the divs", () => {
+    const renderedText = render.find("div");
+    expect(renderedText.length).toEqual(6);
+     });
+    it("displays the Form", () => {
+    const renderedText = render.find("Form");
+    expect(renderedText.length).toEqual(1);
+    });
+    it("displays all the FormGroups", () => {
+    const renderedText = render.find("FormGroup");
+    expect(renderedText.length).toEqual(5);
+    });
+    it("displays all the options", () => {
+    const renderedText = render.find("option");
+    expect(renderedText.length).toEqual(5);
+    });
+});
