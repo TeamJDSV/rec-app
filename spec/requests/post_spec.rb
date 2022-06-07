a43d0967832d7d9acfde47a400e55d687b8bfaf1
aVictorWXYZ <victorargote@gmail.com>
2022-06-07 11:49:14 -0700

    Fixed the post_spec to pass and removed user_helper file

--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -3,16 +3,19 @@ require 'rails_helper'
 RSpec.describe "Posts", type: :request do
   describe "GET /index" do
     it "gets a list of posts" do
-      Post.create(
+      user1 = User.create email: 'test12@test.com', password: 'p2assword', password_confirmation: 'p2assword', name: 'BigBodyS2am', picture: 'https://i1.sndcdn.com/avatars-si6VMrH1nO5JwyQe-jB5sJw-t500x500.jpg', userLocation:'San Di2ego', aboutme:"Tha2t's all." 
+
+      work = Post.create(
         location: "Queen Bee's", 
         review:"LIT AF!?", 
         star_rating: 5, 
         picture:"https://lh3.googleusercontent.com/z0QkuV4prYyiiqJ_iA-vvQZgSiJ_CwOV7Xv7XSAEpqPLJH4Wv5UFlQsrQkMDL6wiea9-JymoGWGCdgK3UUBs_i1O2MZrm2vbN08b2jrk4fijllYbRV4egZxPIy7zFWqCF_yFw06c=w2400", 
         day: "false", 
+        user_id: user1.id 
       )
       get '/posts'
 
-      # post = JSON.parse(response.body)
+      post = JSON.parse(response.body)
       expect(response).to have_http_status(200)
       expect(post.length).to eq 1
     end
@@ -38,19 +41,19 @@ RSpec.describe "Posts", type: :request do
 
   describe "Delete /destroy" do
     it "deletes a post from the directory" do
-      post_params = {
-        post:   {
-          location: "Queen Bee's", 
-          review:"LIT AF!?", 
-          star_rating: 5, 
-          picture:"https://lh3.googleusercontent.com/z0QkuV4prYyiiqJ_iA-vvQZgSiJ_CwOV7Xv7XSAEpqPLJH4Wv5UFlQsrQkMDL6wiea9-JymoGWGCdgK3UUBs_i1O2MZrm2vbN08b2jrk4fijllYbRV4egZxPIy7zFWqCF_yFw06c=w2400", 
-          day: "false", 
-        }
-      }
-      post"/posts", params:post_params
+      user1 = User.create email: 'test12@test.com', password: 'p2assword', password_confirmation: 'p2assword', name: 'BigBodyS2am', picture: 'https://i1.sndcdn.com/avatars-si6VMrH1nO5JwyQe-jB5sJw-t500x500.jpg', userLocation:'San Di2ego', aboutme:"Tha2t's all." 
+
+        Post.create(
+        location: "Queen Bee's", 
+        review:"LIT AF!?", 
+        star_rating: 5, 
+        picture:"https://lh3.googleusercontent.com/z0QkuV4prYyiiqJ_iA-vvQZgSiJ_CwOV7Xv7XSAEpqPLJH4Wv5UFlQsrQkMDL6wiea9-JymoGWGCdgK3UUBs_i1O2MZrm2vbN08b2jrk4fijllYbRV4egZxPIy7zFWqCF_yFw06c=w2400", 
+        day: "false", 
+        user_id: user1.id 
+      )
       post = Post.first
 
-      delete"/posts/#{post.id}", params: post_params
+      delete"/posts/#{post.id}"
 
       expect(response).to have_http_status(200)
 
